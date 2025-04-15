af35c1556972603a9180310fa09f26fc00154da6
Marialafu <lafuentelhmaria@gmail.com>
2025-04-15 18:39:18 +0200

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,40 @@ const selectAleatoryWord = () => {
   const aleatoryWord = allWords[aleatoryNumber];
   return aleatoryWord;
 };
+console.log(selectAleatoryWord());
+
 
 const createGameBoard = () => {
-  const boxGroup = document.createElement('div');
-  for (let i = 0; i < selectAleatoryWord().length; i++) {
-    const boxElement = document.createElement('div');
-    boxElement.classList.add('box');
-    boxGroup.append(boxElement);
+  for (let i = 0; i < 5; i++){
+    const boxGroup = document.createElement('div');
+    boxGroup.classList.add('container')
+   for (let i = 0; i < selectAleatoryWord().length; i++) {
+      const boxElement = document.createElement('div');
+      boxElement.classList.add('box');
+      boxGroup.append(boxElement);
+    }
+    gameBoardElement.append(boxGroup);
   }
-  gameBoardElement.append(boxGroup);
-};
+}
 createGameBoard();
+
+const includeLettersInBox = () => {
+
+    for (let i = 0; i < inputBarElement.value.length; i++){
+      gameBoardElement.children[0].children[i].textContent = inputBarElement.value[i]
+    }
+}
+
+const includeWord = () => {
+  console.log(gameBoardElement.children[0]);
+  
+  if (event.key === 'Enter' && inputBarElement.value.length === gameBoardElement.children[0].length){
+    includeLettersInBox()
+    }
+    
+    
+  }
+  
+
+
+document.addEventListener('keypress', includeWord)
