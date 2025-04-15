136dca92b0e6614841301bf112eb4469e40aa2f7
Marialafu <lafuentelhmaria@gmail.com>
2025-04-15 23:42:49 +0200

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,9 @@ const inputBarElement = document.getElementById('input-bar');
 const gameBoardElement = document.getElementById('game-board');
 const formElement = document.getElementById('form')
 
+//intentos como variable para poder cambiarlo?
 const allWords = ['zara', 'hola'];
+const characters = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'
 
 const selectAleatoryWord = () => {
   const aleatoryNumber = Math.floor(Math.random() * allWords.length);
@@ -28,21 +30,40 @@ createGameBoard();
 
 const includeLettersInBox = () => {
   let inputWordLength = inputBarElement.value.length
+  let childrenValue = 0;
   for (let i = 0; i < inputWordLength; i++){
-    gameBoardElement.children[0].children[i].textContent = inputBarElement.value[i]
+    //como cambiar el children según la palabra ya esté completa
+    gameBoardElement.children[childrenValue].children[i].textContent = inputBarElement.value[i]
+    childrenValue++
   }
 }
 
 const includeWord = (event) => {
   event.preventDefault()
+  const wordLength = gameBoardElement.children[0].childElementCount
 
-  if (inputBarElement.value.length === gameBoardElement.children[0].childElementCount){
+  if (inputBarElement.value.length === wordLength){
     includeLettersInBox()
   } else {
     const wrongMessage = document.createElement('p');
-    wrongMessage.textContent = `Word must have ${inputBarElement.value.length} letters`
+    wrongMessage.textContent = `Word must have ${wordLength} letters`
     gameBoardElement.append(wrongMessage);
-  } 
+  }
+  confirmValidateCharacters()
 }
 
-document.addEventListener('submit', includeWord)
+const confirmValidateCharacters = () => {
+  //cambiar según el children
+  const boxElement = gameBoardElement.children[0].children[0]
+  
+  
+  for (const character of inputBarElement.value.toUpperCase()){  
+    if (characters.includes(character)){
+      boxElement.classList.add('box-yellow');
+      console.dir(boxElement)
+    }
+  }
+  
+}
+
+formElement.addEventListener('submit', includeWord)
