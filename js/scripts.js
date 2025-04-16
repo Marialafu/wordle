a42157ba37fecbcfbfab57ce22c79e25ccd2c1f2
Marialafu <lafuentelhmaria@gmail.com>
2025-04-16 22:45:56 +0200

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@ const inputBarElement = document.getElementById('input-bar');
 const gameBoardElement = document.getElementById('game-board');
 const formElement = document.getElementById('form');
 const messageElement = document.getElementById('message');
+const winOrLose = document.getElementById('win-or-lose')
 
 const allWords = ['zara'];
-const characters = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ';
 
 let secretWord = '';
 let tries = 5;
@@ -42,42 +42,62 @@ const includeLettersInBox = () => {
 
 const includeWord = event => {
   event.preventDefault();
+  
 
   if (inputBarElement.value.length === secretWord.length) {
     messageElement.textContent = '';
+    //sumando aquí la row no funciona se sigue pintando
   } else {
     messageElement.textContent = `Word must have ${secretWord.length} letters`;
+    //poner que no pase a la siguiente fila, que se mantenga en esa.
   }
+  
   confirmValidateCharacters();
   event.target.reset();
 };
 
 const confirmValidateCharacters = () => {
-  const charactersSecretWord = secretWord.split('');
-  const charactersInputWord = inputBarElement.value.split('');
 
   for (let i = 0; i < secretWord.length; i++) {
-    //letra correcta
-    if (charactersInputWord[i] === charactersSecretWord[i]) {
+    //si el cambio de letra se ejecuta dentro del if no funciona
+    //si es fuera tampoco
+    //fuera del for no entiende el i
+    if (inputBarElement.value[i] === secretWord[i]) {
       gameBoardElement.children[currentRow].children[i].classList.add(
         'box-green'
       );
-    } //BUCLE para leer la palabra
-    else if (
-      //la letra no está puesta ya
-      secretWord.includes(charactersInputWord[i])
-    ) {
-      gameBoardElement.children[currentRow].children[i].classList.add(
-        'box-yellow'
-      );
-    } else {
-      gameBoardElement.children[currentRow].children[i].classList.add(
-        'box-grey'
-      );
-    }
+    //muy enrevesado, replace no funciona con un string.
+    let dividedSecretWord = secretWord.split('')
+    dividedSecretWord[i] = '*'
+    secretWord = dividedSecretWord.join('')
+    } 
+  }
+  //como decirle que la que está rosa no la toque
+  for (let i = 0; i < secretWord.length; i++){
+  if (secretWord.includes(inputBarElement.value[i])) {
+  gameBoardElement.children[currentRow].children[i].classList.add(
+  'box-yellow')
+  } else {
+   gameBoardElement.children[currentRow].children[i].classList.add(
+   'box-grey');
+   }
   }
 
   includeLettersInBox();
+  defineIfWinOrLose();
 };
+
+
+const defineIfWinOrLose = () => {
+  
+  if (inputBarElement.value === secretWord){
+    winOrLose.textContent = 'YOU WIN'
+    formElement.remove()
+  } else if (currentRow >= tries){
+    winOrLose.textContent = 'YOU LOSE'
+    formElement.remove()
+  }
+  
+}
 
 formElement.addEventListener('submit', includeWord);
